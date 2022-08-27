66470bfe8570c935a01c26439c4d86f19a3c8f29
MAJIDKIUMBHAR <kumbharmajid200@gmail.com>
2022-08-27 15:53:41 +0500

    Add files via upload

---
+++ first_calculate.py
@@ -0,0 +1,46 @@
+bol=bool(True or False)
+bol_2=bool(True or False)
+while bol==True:
+    bol=bool(True or False)
+    bol_2=bool(True or False)
+
+    num_1=int(input("\nEnter any number :"))
+    operator=str(input("\nEnter any operator : "))
+    if operator=='/' or operator=="*" or operator=="-" or operator=="+":
+
+        num_2=int(input("\nEnter any number :"))
+    
+        if operator == '+':
+            result=num_1+num_2    
+
+        elif operator == '-':
+            result=num_1-num_2
+
+        elif operator == '/':
+            result=num_1/num_2
+
+        elif operator == '*':
+            result=num_1*num_2
+
+        print("\n",num_1,operator,num_2,'=',result)
+
+    else:
+
+        print("\nInvalid operator try again ")
+    
+    while bol_2==True:
+
+        decission=input("\nIf you want to continue then typy yes :")
+        if decission=="yes":
+            decission="yes"
+            bol=True
+            bol_2=False
+    
+        elif decission=="no":
+            print("\nHave A Good Day BYE :)")
+            bol_2=False
+            bol=False
+        
+        else :
+            print("\nPlease Refill \"yes\" or \"no\" :)")
+            bol_2==False
